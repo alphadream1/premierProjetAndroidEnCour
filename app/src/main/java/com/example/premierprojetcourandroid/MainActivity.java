e3b6123fa6d40b6eadb0256f1db0d48348740ae0
AlphaDream1 <philippe.ferraris@gmail.com>
2019-03-27 23:59:00 +0100

    evolution et commit de fin de journée

--- app/src/main/java/com/example/premierprojetcourandroid/MainActivity.java
+++ app/src/main/java/com/example/premierprojetcourandroid/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.premierprojetcourandroid;
 
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -16,12 +18,21 @@ import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, TimePickerDialog.OnTimeSetListener, DatePickerDialog.OnDateSetListener {
 
-    // déclaration composant
+    private static final int ITEM_ID_AD = 1;
+    private static final int ITEM_ID_TP = 2;
+    private static final int ITEM_ID_DP = 3;
+
+    // exo supplementaire
+    private static final SimpleDateFormat FORMAT = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+
+    // 1) déclaration composant
     private Button btValider, btAnnuler;
     private RadioGroup monRadioGroup;
     private RadioButton rbJaime, rbJaimePas;
@@ -29,12 +40,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private ImageView iv_android;
     private Button btNextScreen;
 
+
+    // exo supplementaire
+    //Données
+    private Calendar dateChoisi;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // 2 je recupère l'instance
+        // 2) je recupère l'instance
         btValider = findViewById(R.id.btValider);
         btAnnuler = findViewById(R.id.btAnnuler);
         monRadioGroup = findViewById(R.id.monRadioGroup);
@@ -44,10 +60,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         rbJaimePas = findViewById(R.id.rbJaimePas);
         btNextScreen = findViewById(R.id.btNextScreen);
 
-        // 3 je m'abonne au click
+        // 3) je m'abonne au click
         btValider.setOnClickListener(this);
         btAnnuler.setOnClickListener(this);
         btNextScreen.setOnClickListener(this);
+
+        // exo supplementaire
+        // recup les dates et heure du datepicker et timepicker
+        dateChoisi = Calendar.getInstance();
+
     }
 
     @Override
@@ -73,22 +94,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Intent intent = new Intent(this, SecondActivity.class);
             intent.putExtra("maCle", etSaisirVotreNom.getText().toString());
             startActivity(intent);
+            finish();
         }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        menu.add(0, 1, 0, "AlertDialog");
-        menu.add(0, 2, 0, "TimePicker");
-        menu.add(0, 3, 0, "DatePicker");
+        menu.add(0, ITEM_ID_AD, 0, "AlertDialog");
+        menu.add(0, ITEM_ID_TP, 0, "TimePicker");
+        menu.add(0, ITEM_ID_DP, 0, "DatePicker");
         return super.onCreateOptionsMenu(menu);
     }
 
-    @Override
+   /* @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case 1:
-                //remplir ici
+                AlertDialog.Builder alertDialogBuider = new AlertDialog.Builder(this);
+                alertDialogBuider.setMessage("voici mon alert");
+                alertDialogBuider.setTitle("mon alert");
+                alertDialogBuider.setPositiveButton("ok",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(MainActivity.this, "click sur ok", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                alertDialogBuider.show();
                 break;
             case 2:
                 TimePickerDialog timePickerDialog = new TimePickerDialog(this, this, 12, 30, true);
@@ -101,16 +133,92 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }*/
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == ITEM_ID_TP) {
+            //Ici je lance le TimePicker
+            TimePickerDialog timePickerDialog = new TimePickerDialog(this, this, 14, 33, true);
+            timePickerDialog.show();
+
+        } else if (item.getItemId() == ITEM_ID_DP) {
+            //Ici je lance le DatePicker
+            //Gestion de la date
+            Calendar calendar = Calendar.getInstance();
+//Création de la fenêtre
+//Pour le callback -> Alt+entree -> implémente méthode -> Génère la méthode onTimeSet
+            DatePickerDialog datePickerDialog = new DatePickerDialog(this, this,
+                    calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH),
+                    calendar.get(Calendar.DAY_OF_MONTH));
+//Afficher la fenêtre
+            datePickerDialog.show();
+
+        } else if (item.getItemId() == ITEM_ID_AD) {
+            //Ici je lance l'AlertDialog
+            //Préparation de la fenêtre
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+//Message
+            alertDialogBuilder.setMessage("Mon message");
+//titre
+            alertDialogBuilder.setTitle("Mon titre");
+//bouton ok
+            alertDialogBuilder.setPositiveButton("ok",
+                    new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            //Affiche un toast apres le click sur le bouton ok
+                            Toast.makeText(MainActivity.this, "Click sur ok",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    });
+//Icone
+            alertDialogBuilder.setIcon(R.mipmap.ic_launcher);
+//Afficher la fenêtre
+            alertDialogBuilder.show();
+
+        }
+
+
+        return super.onOptionsItemSelected(item);
     }
 
+    //-------------------
+    //Callback timePicker
+    //-------------------
     @Override
     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        Toast.makeText(MainActivity.this, "Heure choisi " + hourOfDay + " : " + minute, Toast.LENGTH_SHORT).show();
 
+        // exo supplementaire
+        dateChoisi.set(Calendar.HOUR_OF_DAY, hourOfDay);
+        dateChoisi.set(Calendar.MINUTE, minute);
+        String maDate = FORMAT.format(dateChoisi.getTime());
+        Toast.makeText(this, maDate, Toast.LENGTH_SHORT).show();
     }
 
+    //--------------------
+    // Callback Datepicker
+    //--------------------
     @Override
     public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        // premiere option
+        Toast.makeText(MainActivity.this, "Date choisi " + dayOfMonth + "/" + (month + 1) + "/" + year, Toast.LENGTH_SHORT).show();
 
+
+        // tp sup car fini plus tot => utilisé le SimpleDateFormat !!!
+        dateChoisi.set(Calendar.YEAR, year);
+        dateChoisi.set(Calendar.MONTH, month);
+        dateChoisi.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+        String maDate = FORMAT.format(dateChoisi.getTime());
+        Toast.makeText(this, maDate, Toast.LENGTH_LONG).show();
+        // je ne sais pas ce qui est le mieux mais la 2eme genere plus de code!!!
     }
+
+    //--------------------
+    //Private
+    //--------------------
+
 }
 
---
+++ app/src/main/java/com/example/premierprojetcourandroid/MyBroadcast.java
@@ -0,0 +1,17 @@
+package com.example.premierprojetcourandroid;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.Locale;
+
+public class MyBroadcast extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.w("TAG_", Locale.getDefault().getDisplayLanguage());
+        Toast.makeText(context, Locale.getDefault().getDisplayLanguage(), Toast.LENGTH_SHORT).show();
+    }
+}
