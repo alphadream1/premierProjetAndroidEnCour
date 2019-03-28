46467ba8ac2f09fdd749a34407602b47b785efbc
AlphaDream1 <philippe.ferraris@gmail.com>
2019-03-28 11:52:18 +0100

    evolution, mise en place d'un service en arriere plan

--- app/src/main/java/com/example/premierprojetcourandroid/MyService.java
+++ app/src/main/java/com/example/premierprojetcourandroid/MyService.java
@@ -4,13 +4,43 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MyService extends Service {
+
+    private Timer timer;
+
     public MyService() {
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        // methode appelée pour la connexion au service, ignorer pour le moment
+        // throw new UnsupportedOperationException("Not yet implemented");
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                //executer votre tache
+            }
+        }, 0, 60000);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        timer.cancel();
     }
 }
--- app/src/main/java/com/example/premierprojetcourandroid/ServiceExACtivity.java
+++ app/src/main/java/com/example/premierprojetcourandroid/ServiceExACtivity.java
@@ -1,12 +1,15 @@
 package com.example.premierprojetcourandroid;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class ServiceExACtivity extends AppCompatActivity {
+public class ServiceExACtivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button btStop;
     private TextView tvEcranService;
@@ -16,8 +19,28 @@ public class ServiceExACtivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service_ex);
+
+        //
         btStop = findViewById(R.id.btStop);
         tvEcranService = findViewById(R.id.tvEcranService);
         btStart = findViewById(R.id.btStart);
+
+        // ecoute des bouton
+        btStart.setOnClickListener(this);
+        btStop.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        if (v == btStart) {
+            // lance le service
+            startService(new Intent(this, MyService.class));
+            Log.w("Tag_", "Service activer");
+        } else if (v == btStop) {
+            // arrete le service
+            stopService(new Intent(this, MyService.class));
+            Log.w("Tag_", "Service désactiver");
+        }
     }
 }
